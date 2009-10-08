0494d2efaaaa1aa2f11cc79a7bf8ca2a4321dd81
Dan Hackney <dan@haxney.org>
2009-10-07 22:40:27 -0400

    Refactor more functionality into functions.
    Makes the main logic of the program simpler.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- yt-bulk-py.py
+++ yt-bulk-py.py
@@ -9,53 +9,54 @@ from sys import argv
 import xdg.BaseDirectory as bd
 import ConfigParser
 
-if len(argv) != 2:
-    print "Usage: yt-bulk-py video_list"
-    exit(1)
+yt_service = gdata.youtube.service.YouTubeService()
 
-resource_name = 'yt-bulk-py'
+def load_config():
+    resource_name = 'yt-bulk-py'
 
-if not bd.load_first_config(resource_name):
-    sys.stderr.write('Creating config directory: ' + bd.save_config_path(resource_name))
+    if not bd.load_first_config(resource_name):
+        sys.stderr.write('Creating config directory: ' + bd.save_config_path(resource_name))
 
-conf_dir = bd.load_first_config(resource_name)
-conf_file = os.path.join(conf_dir, 'config')
+    conf_dir = bd.load_first_config(resource_name)
+    conf_file = os.path.join(conf_dir, 'config')
 
-conf_skel = """# Configuration file for yt-bulk-py
+    conf_skel = """# Configuration file for yt-bulk-py
 
 # Account information for the user doing the uploading.
 [user]
 email = address@example.com
-password = secret"""
+password = secret
+"""
 
-if not os.path.isfile(conf_file):
-    with open(conf_file, 'w+b') as f:
-        f.write(conf_skel)
+    if not os.path.isfile(conf_file):
+        with open(conf_file, 'w+b') as f:
+            f.write(conf_skel)
 
-config = ConfigParser.ConfigParser()
-config.read(conf_file)
+    config = ConfigParser.ConfigParser()
+    config.read(conf_file)
 
-if not 'user' in config.sections():
-    sys.stderr.write('Error: No [user] section in config file.')
-    exit(1)
+    if not 'user' in config.sections():
+        sys.stderr.write('Error: No [user] section in config file.')
+        exit(1)
 
-if not 'email' in config.options('user') and 'password' in config.options('user'):
-    sys.stderr.write('Error: Missing "email" or "password" options in config file.')
-    exit(1)
+    if not 'email' in config.options('user') and 'password' in config.options('user'):
+        sys.stderr.write('Error: Missing "email" or "password" options in config file.')
+        exit(1)
+    return (config.get('user', 'email'), config.get('user', 'password'))
 
-yt_service = gdata.youtube.service.YouTubeService()
-yt_service.email = config.get('user', 'email')
-yt_service.password = config.get('user', 'password')
-yt_service.source = 'my-example-application'
-yt_service.auth_service_url = 'https://www.google.com/youtube/accounts/ClientLogin'
-yt_service.developer_key = 'AI39si573dorSANLKW4umMAT7awL4jVnlZSfI_J-U5kFvUnNLNFFruGmIRxutNY4UTjP4y_Qx0mZYTkH1b_bocV4g1rJUr3Z4g'
-yt_service.client_id = 'ytapi-DanHackney-PythonYoutubebat-qa3chdm6-0'
-yt_service.ProgrammaticLogin()
+def login(username, password):
+    yt_service.email = config.get('user', 'email')
+    yt_service.password = config.get('user', 'password')
+    yt_service.source = 'my-example-application'
+    yt_service.auth_service_url = 'https://www.google.com/youtube/accounts/ClientLogin'
+    yt_service.developer_key = 'AI39si573dorSANLKW4umMAT7awL4jVnlZSfI_J-U5kFvUnNLNFFruGmIRxutNY4UTjP4y_Qx0mZYTkH1b_bocV4g1rJUr3Z4g'
+    yt_service.client_id = 'ytapi-DanHackney-PythonYoutubebat-qa3chdm6-0'
+    yt_service.ProgrammaticLogin()
 
-print "logged in"
+    print "logged in"
 
-# Load the contents of dirname(<basepath>)/<name>
 def local_meta_info(basepath, name):
+    """Load the contents of dirname(<basepath>)/<name>."""
     meta_path = os.path.join(os.path.dirname(basepath), name)
     with open(meta_path) as f:
         meta_text = f.read()
@@ -86,6 +87,18 @@ def upload_one_video(filename):
     new_entry = yt_service.InsertVideoEntry(video_entry, filename)
     return new_entry
 
+def print_upload_status(status):
+    if upload_status is not None:
+        video_upload_state = upload_status[0]
+        detailed_message = upload_status[1]
+        print "Uploaded state:", video_upload_state, "Detailed status:", detailed_message, "URL:", entry.media.player.url
+    else:
+        print "URL:", entry.media.player.url
+
+if len(argv) != 2:
+    print "Usage: yt-bulk-py video_list"
+    exit(1)
+
 file_list = argv[-1]
 
 with open(file_list) as f:
@@ -95,14 +108,7 @@ while videos:
     f = videos.pop()
     print "Uploading", os.path.basename(f), "..."
     entry = upload_one_video(f)
-    upload_status = yt_service.CheckUploadStatus(entry)
-
-    if upload_status is not None:
-        video_upload_state = upload_status[0]
-        detailed_message = upload_status[1]
-        print "Uploaded state:", video_upload_state, "Detailed status:", detailed_message, "URL:", entry.media.player.url
-    else:
-        print "URL:", entry.media.player.url
+    print_upload_status(yt_service.CheckUploadStatus(entry))
 
     with open(file_list, 'w+b') as l:
         l.write("\n".join(videos))
